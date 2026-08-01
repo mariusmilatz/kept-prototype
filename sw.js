6460163ccc0bce75560dc2d0bdb8a0cc1c530976
Marius Milatz <roundcirclefilmsproduction@gmail.com>
2026-08-01 16:32:26 +0200

    Add first-run onboarding, on-device persistence, and a You/Settings sheet (sample data + reset)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="kept-v2";
+const C="kept-v3";
 const A=["./","index.html","manifest.webmanifest","icon-180.png","icon-192.png","icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)).catch(()=>{}));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
